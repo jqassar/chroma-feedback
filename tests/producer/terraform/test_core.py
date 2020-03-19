3f21acbedf0685287d1b3243486712c54cc0ac7a
Jerry Qassar <jerry.qassar@resmed.com>
2020-03-18 20:27:50 -0700

    Fix terraform producer test.

--- tests/producer/terraform/test_core.py
+++ tests/producer/terraform/test_core.py
@@ -1,14 +1,15 @@
 import os
 import pytest
-from chroma_feedback.producer.gitlab.core import fetch
+from chroma_feedback.producer.terraform.core import fetch
 
 
 def test_fetch_slug() -> None:
-	if 'TFE_TOKEN' in os.environ:
-		result = fetch('https://app.terraform.io', 'redaxmedia/chroma-feedback', os.environ['TFE_TOKEN'])
+	slug = 'tuxco/terraform-cloud-test' # slug = 'redaxmedia/chroma-feedback'
 
+	if 'TFE_TOKEN' in os.environ:
+		result = fetch('https://app.terraform.io', slug, os.environ['TFE_TOKEN'])
 		assert result[0]['producer'] == 'terraform'
-		assert result[0]['slug']
+		assert result[0]['slug'] == slug
 		assert result[0]['active'] is True
 		assert result[0]['status']
 		assert result[0]['effect']
